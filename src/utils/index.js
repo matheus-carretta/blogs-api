5640b40760acfd71468fb37782cf71505b9d1406
Matheus <matheuscarretta@gmail.com>
2022-05-20 22:58:48 -0300

    feat: adiciona funcao para gerar token jwt

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const errorHandler = (status, message) => {
   const newError = {
     status,
@@ -7,6 +10,18 @@ const errorHandler = (status, message) => {
   return newError;
 };
 
+const generateToken = (info) => {
+  const secret = process.env.JWT_SECRET;
+  const jwtConfig = {
+    expiresIn: '1d',
+    algorithm: 'HS256',
+  };
+  const token = jwt.sign({ data: info }, secret, jwtConfig);
+
+  return token;
+};
+
 module.exports = {
   errorHandler,
+  generateToken,
 };
