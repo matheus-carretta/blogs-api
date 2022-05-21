a3f6ca7e0fb9a8f6bcc9fc3f512b8de3d98f75c8
Matheus <matheuscarretta@gmail.com>
2022-05-21 17:08:20 -0300

    feat: cria middleware para validar token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const { errorHandler } = require('../utils');
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
+
+const validateToken = (req, _res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) throw errorHandler(401, 'Token not found');
+
+  try {
+    jwt.verify(token, secret);
+  } catch (err) {
+    console.log(err);
+    throw errorHandler(401, 'Expired or invalid token');
+  }
+
+  next();
+};
+
+module.exports = validateToken;
