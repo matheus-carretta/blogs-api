3f63d8e8204eaa60252de1b2a6b1727bf1330356
Matheus <matheuscarretta@gmail.com>
2022-05-22 10:53:42 -0300

    feat: cria a model Category

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,23 +1,18 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     }
-  };
-  Category.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Category',
   });
+
   return Category;
-};
+}
+
+module.exports = Category;
